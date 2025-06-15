2287d06fb4de613fdb5a216d970f2ad9c30a8b10
Harshit Kumar <kharshit242@gmail.com>
2025-06-15 16:11:03 +0530

    api key added

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+API_KEY = os.getenv('TMDB_API_KEY')
 
 def fetch_poster(movie_id):
     url = f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}&language=en-US'
